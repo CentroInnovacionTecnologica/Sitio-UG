3f774003dc70bf11fbf60d0b049cddf3506ec3f3
richmtx <martinezhernandezricardo06@gmail.com>
2026-06-22 11:48:29 -0600

    Commit

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,16 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { CATALOGO } from './components/productos/data/catalogo';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'productos/:categoria',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      return CATALOGO.map((c) => ({ categoria: c.slug }));
+    },
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
